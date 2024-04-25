871dc613a0f7cf054cb15e87f73fefe802c8044c
Igorpereirag <igorpereira1501@gmail.com>
2024-04-25 15:47:54 -0300

    fix

--- uml_iphone/Iphone.java
+++ uml_iphone/Iphone.java
@@ -6,44 +6,49 @@ public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorI
 
     @Override
     public void tocarMusica() {
-        
+        musicaTocando = true;
+        System.out.println("Música tocando...");
     }
 
     @Override
     public void pausarMusica() {
-      
+        musicaTocando = false;
+        System.out.println("Música pausada.");
     }
 
     @Override
     public void selecionarMusica() {
-        
+        System.out.println("Selecionando música...");
     }
 
     @Override
     public void ligar() {
-       
+        ligacaoAtiva = true;
+        System.out.println("Ligação ativa.");
+    }
 
     @Override
     public void atender() {
-       
+        System.out.println("Atendendo chamada...");
     }
 
     @Override
     public void iniciarCorreioVoz() {
-        
+        System.out.println("Iniciando correio de voz...");
     }
 
     @Override
     public void exibirPagina() {
-       
+        navegando = true;
+        System.out.println("Exibindo página...");
     }
 
     @Override
     public void adicionarNovaAba() {
-        
+        System.out.println("Adicionando nova aba...");
+    }
 
     @Override
     public void atualizarPagina() {
-        
+        System.out.println("Atualizando página...");
     }
-}
---
+++ uml_iphone/main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        iPhone iphone = new iPhone();
+        iphone.tocarMusica();
+        iphone.selecionarMusica();
+        iphone.pausarMusica();
+
+        iphone.ligar();
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+
+        iphone.exibirPagina();
+        iphone.adicionarNovaAba();
+        iphone.atualizarPagina();
+    }
+}
