488c0a65fc59fa71586a017d3a41122b8145bddb
Igorpereirag <igorpereira1501@gmail.com>
2024-04-25 15:44:03 -0300

    uml iphone interfaces

---
+++ uml_iphone/Iphone.java
@@ -0,0 +1,49 @@
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    private boolean musicaTocando;
+    private String musicaAtual;
+    private boolean ligacaoAtiva;
+    private boolean navegando;
+
+    @Override
+    public void tocarMusica() {
+        
+    }
+
+    @Override
+    public void pausarMusica() {
+      
+    }
+
+    @Override
+    public void selecionarMusica() {
+        
+    }
+
+    @Override
+    public void ligar() {
+       
+
+    @Override
+    public void atender() {
+       
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        
+    }
+
+    @Override
+    public void exibirPagina() {
+       
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        
+
+    @Override
+    public void atualizarPagina() {
+        
+    }
+}
